1502967014953e5131d5fb324c4266ff7156b978
unknown <zainbac@gmail.com>
2020-08-26 00:05:38 +0800

    deploy appss

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
+// app.get('/', function (req, res) {
+//   res.send('Hello Worlddd');
+// });
 app.get('/', function (req, res) {
-  res.send('Hello Worlddd');
-});
-app.get('/api/courses', (req, res) => {
   res.send([
     {
       title: ' ',
