05942b6eac2cdae1fc915d551513b0a4bef5f0e1
unknown <zainbac@gmail.com>
2020-09-16 00:55:20 +0800

    try

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ app.get('/', function (req, res) {
   res.send([
     {
       name: 'Muhammad khazaain',
-      email: 'miyah.myles@gmail.com',
+      nophone: '0134472956',
       position: 'Tecnician Area Kuala Lumpur',
       photo: 'https://i.ibb.co/jyJ1Hdk/pilot1.png',
     },
     {
       name: 'Arif Ariffin',
-      email: 'june.cha@gmail.com',
+      nophone: '0134472956',
       position: 'Tecnician Area Kuala Lumpur',
       photo: 'https://i.ibb.co/yqMpY2Y/pilot2.png',
     },
     {
       name: 'Muhammad Fad',
-      email: 'iida.niskanen@gmail.com',
+      nophone: '0134472956',
       position: 'Tecnician Area Kuala Lumpur',
       photo: 'https://i.ibb.co/QF07LBb/pilot-3.png',
     },
     {
       name: 'Ricie neilson',
-      email: 'renee.sims@gmail.com',
+      nophone: '0134472956',
       position: 'Tecnician Area Kuala Lumpur',
       photo: 'https://i.ibb.co/H4Bt05z/pilot-4.png',
     },
